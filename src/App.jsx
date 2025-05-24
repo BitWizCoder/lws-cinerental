ee25efb354ad8c78d34e53cd97b1b39a3230b23b
Muhammad Noman <mhmdnoman01@gmail.com>
2025-05-25 01:16:28 +0600

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Header from "./components/Header";
+import MovieList from "./components/MovieList";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
