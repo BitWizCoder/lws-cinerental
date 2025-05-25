94102c6f51305969d981200b0df4e07d758d004e
Muhammad Noman <mhmdnoman01@gmail.com>
2025-05-25 14:29:19 +0600

    getig data from context

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { useContext } from "react";
 import { getImageUrl } from "../utils/cine-utility";
 import Rating from "./Rating";
 import { useState } from "react";
 import MovieDetailsModal from "./MovieDetailsModal";
+import { MovieContext } from "../context";
 
 export default function MovieCard({ movie }) {
   const [showModal, SetShowModal] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const { cartData, setCartData } = useContext(MovieContext);
+
+  function handleAddToCart(event, movie) {
+    event.stopPropagation();
+
+    const found = cartData.find((item) => {
+      return item.id === movie.id;
+    });
+
+    if (!found) {
+      setCartData([...cartData, movie]);
+    } else {
+      console.error(
+        `The movie ${movie.title} has been already added to the cart.`
+      );
+    }
+  }
 
   function handleModalClose() {
     setSelectedMovie(null);
@@ -21,7 +41,11 @@ export default function MovieCard({ movie }) {
   return (
     <>
       {showModal && (
-        <MovieDetailsModal movie={selectedMovie} onClose={handleModalClose} />
+        <MovieDetailsModal
+          movie={selectedMovie}
+          onClose={handleModalClose}
+          onCartAdd={handleAddToCart}
+        />
       )}
       <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
         <a href="#" onClick={() => handleMovieSelection(movie)}>
@@ -37,6 +61,7 @@ export default function MovieCard({ movie }) {
               <Rating value={movie.rating} />
             </div>
             <a
+              onClick={(e) => handleAddToCart(e, movie)}
               className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
               href="#"
             >
